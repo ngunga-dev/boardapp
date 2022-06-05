8d5e6395919b4f4ed013d40616a9da8002276144
ngunga-dev <ngungadev@gmail.com>
2022-06-05 20:32:04 +0100

    home

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,25 @@
-import styles from "./style.module.scss"
+import Link from "next/link";
+import styles from "./style.module.scss";
 
-export function Header(){
-    return(
-        <header className={styles.headerContainer}>
-            <div className={styles.headerContent}>
-                <img src="/images/logo.svg" alt="Logo do Meu Board" />
-                <a href="">Home</a>
-                <a href="">Meu board</a>
+export function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Link href={"/"}>
+          <img src="/images/logo.svg" alt="Logo do Meu Board" />
+        </Link>
 
-                <button>Entrar com github</button>
-            </div>
-        </header>
-    )
+        <nav>
+          <Link href={"/"}>
+            <a>Home</a>
+          </Link>
+          <Link href={"/board"}>
+            <a>Meu board</a>
+          </Link>
+        </nav>
+
+        <button>Entrar com github</button>
+      </div>
+    </header>
+  );
 }
